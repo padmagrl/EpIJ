3bc3ced840821aa721aa473f8bd31915b62a17da
Padma_Gorrela <padmagrla@gmail.com>
2021-10-15 10:00:42 +0200

    sop1

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String x = "hello";
+        System.out.println(x);
+        String he = "heiiiii baby";
+        System.out.println(he);
+        System.out.println("added new sop");
+
+    }
+}
