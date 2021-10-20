1b371382f88020c2513dc38581dc4f64e14b4eab
Padma_Gorrela <padmagrla@gmail.com>
2021-10-20 11:21:32 +0200

    main2 c

---
+++ src/com/company/Main2.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        String x = "hello";
+        System.out.println(x);
+        String he = "heiiiii baby";
+        String y = "y";
+        System.out.println(he);
+        System.out.println("master");
+        System.out.println("BR1");
+        System.out.println("BR2");
+
+    }
+}
