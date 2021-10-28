91cc8b513009be95f4df1dea9419572d294a5e60
Padma_Gorrela <padmagrla@gmail.com>
2021-10-28 16:16:21 +0200

    ToConflict

--- src/com/company/ToConflict.java
+++ src/com/company/ToConflict.java
@@ -3,6 +3,6 @@ package com.company;
 public class ToConflict {
     public static void main(String[] args) {
         System.out.println(" BR2");
-        System.out.println(" br2 conflict");
+        System.out.println(" br2 conflict in main");
     }
 }
